1168ad9761ddb1f9b66c11b25b40ff417c6ba7bb
Christian <75549110+proprdev@users.noreply.github.com>
2021-05-17 16:23:14 -0500

    do smart things

--- main.m
+++ main.m
@@ -6,12 +6,12 @@ int main()
 	@autoreleasepool 
 	{
 		int grams;
-    	char name[2000];
-    	printf("Who will we be delivering to today?\n==> ");
-    	scanf("%s", name);
-    	printf("How much meth would you like? (grams)\n==> ");
-    	scanf("%d", &grams);
-    	printf("Thanks, %s! Your %d grams of meth will be delivered shortly! \nCopyright (c) Meth Development\n", name, grams);
-    	return (0);
+    		char name[2000];
+    		printf("Who will we be delivering to today?\n==> ");
+    		scanf("%s", name);
+    		printf("How much meth would you like? (grams)\n==> ");
+    		scanf("%d", &grams);
+    		printf("Thanks, %s! Your %d grams of meth will be delivered shortly! \nCopyright (c) Meth Development\n", name, grams);
+    		return 0;
 	}
 }
